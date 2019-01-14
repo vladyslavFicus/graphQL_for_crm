3f821f31f620c5f0bed34664ba0be7ac48e11ae8
Valery Zakharchenko <vzakharchenko@newagesol.com>
2019-01-14 17:53:07 +0200

    fix-hierarchy-tree-query

--- src/utils/hierarchyRequests.js
+++ src/utils/hierarchyRequests.js
@@ -114,7 +114,7 @@ const getBranchHierarchyTree = (uuid, authorization) => {
   return fetch(`${global.appConfig.apiUrl}/trading_hierarchy_updater/branch/hierarchy/${uuid}`, {
     method: 'GET',
     headers: {
-      authorization: 'asd',
+      authorization,
       accept: 'application/json',
       'content-type': 'application/json',
     },
