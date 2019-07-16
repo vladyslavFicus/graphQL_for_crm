6d18174ed4fa4dbb6de20525a5867c46809ac420
Valery Zakharchenko <vzakharchenko@overonix.com>
2019-07-16 14:59:57 +0000

    [FAL-1736] - Changed GET method to search operators on POST

--- src/graphql/common/resolvers/operators.js
+++ src/graphql/common/resolvers/operators.js
@@ -3,6 +3,7 @@ const {
   getOperators: getOperatorsRequest,
   createOperator: createOperatorRequest,
   getOperatorByUUID: getOperatorByUUIDRequest,
+  getOperatorsByUUIDs: getOperatorsByUUIDsRequest,
   updateOperator: updateOperatorRequest,
 } = require('../../../utils/operatorRequests');
 const {
@@ -13,20 +14,9 @@ const Logger = require('../../../utils/logger');
 const { addOperatorToBranch } = require('./hierarchy');
 
 const getOperators = async (_, args, { headers: { authorization }, hierarchy }) => {
-  // Hack: Get all operators and then filter it by hierarchy
-  const _args = { ...args, size: 2000 };
-
-  const operators = await getOperatorsRequest(_args, authorization);
-
-  if (operators.error) {
-    return operators;
-  }
-
   const operatorsIds = await hierarchy.getOperatorsIds();
 
-  const filteredOperators = operators.data.content.filter(operator => operatorsIds.includes(operator.uuid));
-
-  return { data: { ...operators.data, content: filteredOperators, totalElements: filteredOperators.length } };
+  return getOperatorsByUUIDsRequest({ ...args, uuids: operatorsIds }, authorization);
 };
 
 const getOperatorByUUID = async (_, { uuid }, { headers: { authorization }, hierarchy }) => {
