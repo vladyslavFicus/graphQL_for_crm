07d79bd2c006208ee196b946898e9fc0d4da799b
Sergiy Tkachenko <sireks20@gmail.com>
2020-01-21 10:32:07 +0200

    Fix: rever bulk update changes

--- src/graphql/common/resolvers/clients.js
+++ src/graphql/common/resolvers/clients.js
@@ -6,14 +6,6 @@ const { getProfiles, bulkUpdateSalesStasuses, bulkUpdateRetentionStasuses } = re
 
 const CLIENTS_SIZE_LIMIT = 10000;
 
-const bulkUpdate = async (body, request, authorization) => {
-  const { error } = await request(body, authorization);
-
-  if (error) {
-    throw error;
-  }
-};
-
 const bulkRepresentativeUpdate = async (_, args, { headers: { authorization } }) => {
   const {
     allRowsSelected,
@@ -54,61 +46,69 @@ const bulkRepresentativeUpdate = async (_, args, { headers: { authorization } })
     clients = clientsData;
   }
 
-  try {
-    if (salesStatus) {
-      bulkUpdate(
-        {
-          salesStatus,
-          uuids: clients.map(client => client.uuid),
-        },
-        bulkUpdateSalesStasuses,
-        authorization
-      );
-    }
+  if (salesStatus) {
+    const { error } = await bulkUpdateSalesStasuses(
+      {
+        salesStatus,
+        uuids: clients.map(client => client.uuid),
+      },
+      authorization
+    );
 
-    if (retentionStatus) {
-      bulkUpdate(
-        {
-          retentionStatus,
-          uuids: clients.map(client => client.uuid),
-        },
-        bulkUpdateRetentionStasuses,
-        authorization
-      );
+    if (error) {
+      return { error };
     }
+  }
 
-    if (salesRepresentative || retentionRepresentative) {
-      bulkUpdate(
-        {
-          parentUsers: salesRepresentative || retentionRepresentative,
-          userUuids: clients.map(client => client.uuid),
-        },
-        bulkMassAssignHierarchyUser,
-        authorization
-      );
-    }
+  if (retentionStatus) {
+    const { error } = await bulkUpdateRetentionStasuses(
+      {
+        retentionStatus,
+        uuids: clients.map(client => client.uuid),
+      },
+      authorization
+    );
 
-    if (isMoveAction) {
-      bulkUpdate(
-        {
-          assignments: clients.map(client => ({
-            uuid: client.uuid,
-            assignToOperator:
-              type === 'SALES'
-                ? client.acquisition
-                  ? client.acquisition.salesRepresentative
-                  : client.salesRepresentative
-                : client.acquisition
-                ? client.acquisition.retentionRepresentative
-                : client.retentionRepresentative,
-          })),
-        },
-        bulkUpdateHierarchyUser,
-        authorization
-      );
+    if (error) {
+      return { error };
     }
-  } catch (error) {
-    return error;
+  }
+
+  if (salesRepresentative || retentionRepresentative) {
+    const { error } = await bulkMassAssignHierarchyUser(
+      {
+        parentUsers: salesRepresentative || retentionRepresentative,
+        userUuids: clients.map(client => client.uuid),
+      },
+      authorization
+    );
+
+    if (error) {
+      return { error };
+    }
+  }
+
+  if (isMoveAction) {
+    const { error } = await bulkUpdateHierarchyUser(
+      {
+        assignments: clients.map(client => ({
+          uuid: client.uuid,
+          assignToOperator:
+            type === 'SALES'
+              ? client.acquisition
+                ? client.acquisition.salesRepresentative
+                : client.salesRepresentative
+              : client.acquisition
+              ? client.acquisition.retentionRepresentative
+              : client.retentionRepresentative,
+        })),
+      },
+      authorization
+    );
+
+    if (error) {
+      return { error };
+    }
   }
 };
 
