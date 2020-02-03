a5f2846967b6df51e61baeafaa9d5e3a114e4f80
Sergey Tkachenko <stkachenko@overonix.com>
2020-02-03 12:36:15 +0000

    [FS-388] [BO]Graph cant load files for some clients on FILES tab

--- src/graphql/query/NoteType/NoteType.js
+++ src/graphql/query/NoteType/NoteType.js
@@ -5,14 +5,14 @@ const OperatorType = require('../OperatorType');
 const NoteType = new GraphQLObjectType({
   name: 'Note',
   fields: () => ({
-    playerUUID: { type: new GraphQLNonNull(GraphQLString) },
-    pinned: { type: new GraphQLNonNull(GraphQLBoolean) },
+    playerUUID: { type: GraphQLString },
+    pinned: { type: GraphQLBoolean },
     subject: { type: GraphQLString },
-    content: { type: new GraphQLNonNull(GraphQLString) },
-    targetUUID: { type: new GraphQLNonNull(GraphQLString) },
-    changedBy: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: GraphQLString },
+    targetUUID: { type: GraphQLString },
+    changedBy: { type: GraphQLString },
     changedAt: { type: GraphQLString },
-    noteId: { type: new GraphQLNonNull(GraphQLString) },
+    noteId: { type: GraphQLString },
     _id: {
       type: new GraphQLNonNull(GraphQLString),
       resolve: ({ noteId }) => noteId,
