24c1b48a93bade4746dcc92859d5f7c1908d6a7e
Yevhen Savitskyi <ysavitskyi@easytech.co>
2020-08-17 22:33:49 +0300

    [FS-1668] - changed leads acquisition

--- src/Http/Graphql/resolvers/Query/Query.js
+++ src/Http/Graphql/resolvers/Query/Query.js
@@ -235,23 +235,11 @@ module.exports = {
   /**
    * Lead API
    */
-  async leads(_, { args }, { dataSources, brand: { id: brandId } }) {
+  leads(_, { args }, { dataSources, brand: { id: brandId } }) {
     return dataSources.LeadAPI.getLeads({ brandId, ...args });
   },
-  async lead(_, { uuid }, { dataSources }) {
-    const [
-      { salesOperator: salesAgent, ...acquisition },
-      leadProfile,
-    ] = await Promise.all([
-      dataSources.HierarchyAPI.getUserAcquisitionById(uuid), 
-      dataSources.LeadAPI.getLead(uuid),
-    ]);
-
-    return {
-      ...leadProfile,
-      ...acquisition,
-      salesAgent,
-    };
+  lead(_, { uuid }, { dataSources }) {
+    return dataSources.LeadAPI.getLead(uuid);
   },
 
   /**
--- src/Http/Graphql/typeDefs/Common/Lead.js
+++ src/Http/Graphql/typeDefs/Common/Lead.js
@@ -26,5 +26,6 @@ module.exports = gql`
     source: String
     surname: String!
     uuid: String!
+    acquisition: HierarchyUserAcquisition
   }
 `;
