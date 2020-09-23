84d65297ab053c3584fef80791b95e6692d0743b
Valery Zakharchenko <vzakharchenko@newagesol.com>
2020-09-23 10:43:22 +0300

    [FS-1955] - Added 2 parameters to DiDLogic request

--- src/Http/Graphql/dataSources/DidLogicAPI.js
+++ src/Http/Graphql/dataSources/DidLogicAPI.js
@@ -7,11 +7,18 @@ class DidLogicAPI extends RESTDataSource {
    * @param url DidLogic host url
    * @param number Client phone number
    * @param agent Operator SIP number
+   * @param accountId Account ID. Must be used together with `crm`.
+   * @param crm CRM Name.  Must be used together with `account_id`.
    *
    * @return {Promise}
    */
-  createCall(url, number, agent) {
-    return this.post(url, { number, agent });
+  createCall(url, number, agent, accountId, crm) {
+    return this.post(url, {
+      number,
+      agent,
+      account_id: accountId,
+      crm,
+    });
   }
 }
 
