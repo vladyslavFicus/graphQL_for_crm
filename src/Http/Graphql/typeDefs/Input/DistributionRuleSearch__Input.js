ec298f02b730c7177ce96569f97b0a6c9c14e113
Sergey Tkachenko <stkachenko@overonix.com>
2020-10-26 14:14:55 +0200

    [FS-2505] - added an ability to search DistributionRules by multiple executionPeriodsInHours

--- src/Http/Graphql/typeDefs/Input/DistributionRuleSearch__Input.js
+++ src/Http/Graphql/typeDefs/Input/DistributionRuleSearch__Input.js
@@ -14,6 +14,6 @@ module.exports = gql`
     createdDateTo: String
     lastTimeExecutedFrom: String
     lastTimeExecutedTo: String
-    executionPeriodInHours: Int
+    executionPeriodsInHours: [Int]
   }
 `;
