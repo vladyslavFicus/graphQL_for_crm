da0ff7f43370bdf12ee04a2a95c83c8b59293e56
Igor Sorokopud <igorsorokopud@ip-192-168-1-191.eu-central-1.compute.internal>
2020-12-17 13:43:52 +0200

    [FS-2972] - Small fix

--- src/Http/Graphql/resolvers/Mutation/payment/PaymentMutation.js
+++ src/Http/Graphql/resolvers/Mutation/payment/PaymentMutation.js
@@ -80,6 +80,19 @@ module.exports = {
   },
 
   /**
+   * Change original agent
+   *
+   * @param _
+   * @param args
+   * @param dataSources
+   *
+   * @return {Promise}
+   */
+  async changeOriginalAgent(_, args, { dataSources }) {
+    await dataSources.PaymentAPI.changeOriginalAgent(args);
+  },
+
+  /**
    * Change creation time
    *
    * @param _
