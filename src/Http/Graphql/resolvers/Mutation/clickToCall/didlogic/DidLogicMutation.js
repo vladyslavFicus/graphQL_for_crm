db7530b11bccd7ce15a99213d6b3c24da88d5dbc
Valery Zakharchenko <vzakharchenko@easytech.co>
2022-01-17 13:14:28 +0000

    Merged in FS-4242-add-logging-didlogic-click2call-request (pull request #204)
    [FS-4242] - Added logging for didlogic click2call requests
    
    * [FS-4242] - Added logging for didlogic click2call requests
    
    
    Approved-by: Yevhen Marchenko
    Approved-by: Igor Sorokopud

--- src/Http/Graphql/resolvers/Mutation/clickToCall/didlogic/DidLogicMutation.js
+++ src/Http/Graphql/resolvers/Mutation/clickToCall/didlogic/DidLogicMutation.js
@@ -1,5 +1,6 @@
 const { get } = require('lodash');
 const getFieldByType = require('../../../../utils/getFieldByType');
+const Logger = require('../../../../../../lib/Logger');
 
 module.exports = {
   /**
@@ -10,10 +11,11 @@ module.exports = {
    * @param dataSources
    * @param userUUID
    * @param brand
+   * @param requestId
    *
    * @return {Promise<any>}
    */
-  async createCall(_, { uuid, field, type }, { dataSources, userUUID, brand }) {
+  async createCall(_, { uuid, field, type }, { dataSources, userUUID, brand, requestId }) {
     const number = await getFieldByType(uuid, field, type, dataSources);
 
     const { url } = brand.clickToCall.didlogic;
@@ -22,6 +24,22 @@ module.exports = {
 
     const sip = get(operator, 'clickToCall.didlogicPhone');
 
-    await dataSources.DidLogicAPI.createCall(url, number, sip, uuid, 'CRM');
+    Logger.info({ requestId, uuid, number, sip }, 'Before Didlogic click2call request');
+
+    try {
+      await dataSources.DidLogicAPI.createCall(url, number, sip, uuid, 'CRM');
+
+      Logger.info({ requestId, uuid, number, sip }, 'Success after Didlogic click2call request');
+    } catch (e) {
+      Logger.warn({
+        requestId,
+        uuid,
+        number,
+        sip,
+        response: e.extensions.response,
+      }, 'Error after Didlogic click2call request');
+
+      throw e;
+    }
   },
 };
