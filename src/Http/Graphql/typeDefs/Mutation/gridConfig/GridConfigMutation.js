b64d9260aeba74887752bc03fde2c0455d7edcd8
Yevhen Marchenko <ymarchenko@easytech.co>
2022-01-25 09:28:01 +0200

    update update mutaton def

--- src/Http/Graphql/typeDefs/Mutation/gridConfig/GridConfigMutation.js
+++ src/Http/Graphql/typeDefs/Mutation/gridConfig/GridConfigMutation.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 module.exports = gql`
   type GridConfigMutation {
     create(columns: String! type: GridConfig__Types__Enum!): GridConfig
-    update(columns: String!, uuid: String!): GridConfig
+    update(columns: String!, uuid: String!): Boolean
     delete(uuid: String!): Boolean
   }
 `;
