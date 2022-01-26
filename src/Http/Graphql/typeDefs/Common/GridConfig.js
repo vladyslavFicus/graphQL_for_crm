8897f8b68e5a7d2606b1d275929e557364d1d1ed
Yevhen Marchenko <ymarchenko@easytech.co>
2022-01-26 17:38:06 +0200

    update schema

--- src/Http/Graphql/typeDefs/Common/GridConfig.js
+++ src/Http/Graphql/typeDefs/Common/GridConfig.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server-express');
 module.exports = gql`  
   type GridConfig {
     uuid: String
-    columns: String
+    columns: [String]
     type: GridConfig__Types__Enum
   }
 `;
--- src/Http/Graphql/typeDefs/Mutation/gridConfig/GridConfigMutation.js
+++ src/Http/Graphql/typeDefs/Mutation/gridConfig/GridConfigMutation.js
@@ -2,8 +2,8 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
   type GridConfigMutation {
-    create(columns: String! type: GridConfig__Types__Enum!): GridConfig
-    update(columns: String!, uuid: String!): Boolean
+    create(columns: [String]! type: GridConfig__Types__Enum!): GridConfig
+    update(columns: [String]!, uuid: String!): Boolean
     delete(uuid: String!): Boolean
   }
 `;
