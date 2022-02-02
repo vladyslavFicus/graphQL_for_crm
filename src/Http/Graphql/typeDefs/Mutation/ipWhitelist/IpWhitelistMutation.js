564c89b5ef4fbf007b74cb14052e00dc23042384
Yevhen Marchenko <ymarchenko@easytech.co>
2022-02-02 13:11:26 +0000

    Merged in FS-4452-whitelist-description-validation (pull request #211)
    FS-4452 [FE] Add validation to description when add or update ip address
    
    * FS-4452 [FE] Add validation to description when add or update ip address
    
    
    Approved-by: Igor Sorokopud
    Approved-by: Valery Zakharchenko

--- src/Http/Graphql/typeDefs/Mutation/ipWhitelist/IpWhitelistMutation.js
+++ src/Http/Graphql/typeDefs/Mutation/ipWhitelist/IpWhitelistMutation.js
@@ -2,8 +2,8 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
    type IpWhitelistMutation {
-    add(ip: String!, description: String): IpWhitelistAddress 
+    add(ip: String!, description: String!): IpWhitelistAddress 
     delete(uuid: String!): Boolean
-    edit(uuid: String, description: String): IpWhitelistAddress
+    edit(uuid: String, description: String!): IpWhitelistAddress
   }
 `;
