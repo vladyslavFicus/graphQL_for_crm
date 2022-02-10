437f29267798c34ca1a8e4837011a50b6ff429c6
Valery Zakharchenko <vzakharchenko@easytech.co>
2022-02-10 07:56:10 +0000

    Merged in FS-4465-securities (pull request #216)
    [FS-4465] - Securities
    
    * [FS-4465] - Securities

--- src/Http/Graphql/resolvers/Query/tradingEngine/TradingEngineQuery.js
+++ src/Http/Graphql/resolvers/Query/tradingEngine/TradingEngineQuery.js
@@ -2,4 +2,10 @@ module.exports = {
   symbols(_, { args }, { dataSources }) {
     return dataSources.TradingEngineAdminAPI.getSymbols(args);
   },
+  securities(_, __, { dataSources }) {
+    return dataSources.TradingEngineAdminAPI.getSecurities();
+  },
+  security(_, { securityName }, { dataSources }) {
+    return dataSources.TradingEngineAdminAPI.getSecurity(securityName);
+  },
 };
--- src/Http/Graphql/typeDefs/Query/tradingEngine/TradingEngineQuery.js
+++ src/Http/Graphql/typeDefs/Query/tradingEngine/TradingEngineQuery.js
@@ -3,5 +3,7 @@ const { gql } = require('apollo-server-express');
 module.exports = gql`
   type TradingEngineQuery {
     symbols(args: TradingEngineSymbols__Input): TradingEngineSymbolSearch! @pageable
+    securities: [TradingEngineSecurity!]!
+    security(securityName: String!): TradingEngineSecurity
   }
 `;
