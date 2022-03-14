224989702f974ca63a622dd94740881a55ca132e
Yevhen Marchenko <ymarchenko@easytech.co>
2022-03-14 21:24:39 +0200

    fix PR notes

--- src/Http/Graphql/typeDefs/Input/TradingEngineCreateOperator__Input.js
+++ src/Http/Graphql/typeDefs/Input/TradingEngineCreateOperator__Input.js
@@ -2,12 +2,12 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
 input TradingEngineCreateOperator__Input {
-  firstName: String!,
-  lastName: String!,
-  email: String!,
-  phone: String,
-  password: String!,
-  role: String!,
-  groupNames: [String],
+  firstName: String!
+  lastName: String!
+  email: String!
+  phone: String
+  password: String!
+  role: String!
+  groupNames: [String]
 }
 `;
