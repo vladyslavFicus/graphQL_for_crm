02edda7073f75c99c70a7f9aa6cbbe186e8c220a
Valery Zakharchenko <vzakharchenko@easytech.co>
2022-04-01 07:23:52 +0000

    Merged in FS-4714-date-range-search (pull request #255)
    [FS-4714] - Holiday date time range filter
    
    * Merged in FS-4591-deposit-toogle (pull request #236)
    
    FS-4591 added can deposit permission
    
    * FS-4591 added can deposit permission
    
    * Merge branch 'master' of bitbucket.org:easytech-global/backoffice-graphql into FS-4591-deposit-toogle
    
    * FS-4591 renamed f…
    * Merged in FS-4565-delete-ip-whitelist (pull request #242)
    
    FS-4565 delete ip whitelist
    
    * FS-4565 new end point
    
    * FS-4565 new end point
    
    
    Approved-by: Yevhen Marchenko
    Approved-by: Valery Zakharchenko
    * Merged in FS-4687-extend-new-operator-flow (pull request #247)
    
    FS-4687 [FE] Extend dealing operator creation modal flow
    
    * FS-4687 [FE] Extend dealing operator creation modal flow
    
    
    Approved-by: Danil Sitdikov
    Approved-by: Valery Zakharchenko
    * Merged in FS-4732-fix-delete-many (pull request #248)
    
    FS-4732 [FE] "Boolean cannot represent a non boolean value: \"\"" error when bulk deleting IP's
    
    * FS-4732 [FE] "Boolean cannot represent a non boolean value: \"\"" error when bulk deleting IP's
    * Merged in FS-4726-deposit-brand-config (pull request #251)
    
    FS-4726 deposit brand config
    
    * FS-4726 deposit brand config
    
    * fix: fixed PR
    
    * fixed typo
    
    
    Approved-by: Valery Zakharchenko
    * Merged in FS-4733-ipwhitelist-fix (pull request #253)
    
    FS-4733 [FE] Checkboxes remaines marked after successfull ip's deletion
    
    * FS-4733 [FE] Checkboxes remaines marked after successfull ip's deletion
    FS-4734 [FE] Click on "Mark all" checkboxes btn does n…
    * [FS-4714] - Holiday date time range filter
    
    
    Approved-by: Yevhen Marchenko

--- src/Http/Graphql/typeDefs/Input/TradingEnginHolidays__Input.js
+++ src/Http/Graphql/typeDefs/Input/TradingEnginHolidays__Input.js
@@ -1,8 +1,14 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  input TradingEngineHolidays__DateTimeRange__Input {
+    from: String
+    to: String
+  }
+
   input TradingEngineHolidays__Input {
     description: String
+    dateTimeRange: TradingEngineHolidays__DateTimeRange__Input
     page: Page__Input
   }
 `;
