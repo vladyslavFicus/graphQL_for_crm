44461c0f1e559cf7bf6ededd2878fe32cdf44c24
Valery Zakharchenko <vzakharchenko@easytech.co>
2022-04-20 12:54:18 +0000

    Merged in FS-4870-add-search-filter-by-status-name (pull request #269)
    [FS-4870] - Add search filter by status name
    
    * [FS-4870] - Add search filter by status name
    
    
    Approved-by: Yevhen Marchenko

--- src/Http/Graphql/typeDefs/Input/AcquisitionStatuses__Input.js
+++ src/Http/Graphql/typeDefs/Input/AcquisitionStatuses__Input.js
@@ -2,6 +2,7 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
   input AcquisitionStatuses__Input {
+    statusName: String
     type: AcquisitionStatusTypes__Enum
   }
 `;
